cad02849ba91f8137866f2060f4be5dcca877533
san5231 <scchen17@gmail.com>
2018-10-30 00:52:10 -0700

    install caches and fetch

--- sw.js
+++ sw.js
@@ -1,3 +1,52 @@
+const staticCacheName = 'restaurant-review-cache-v1'
+
+self.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(caches){
+			return caches.addAll([
+				'index.html',
+                'restaurant.html',
+  				'css/styles.css',               
+                'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'data/restaurants.json',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg'                
+  			]);
+		})
+	)
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function(cacheName) {
+            return (
+              cacheName.startsWith("restaurant-") && cacheName != staticCacheName
+            );
+          })
+          .map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
-  console.log(event.request);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
